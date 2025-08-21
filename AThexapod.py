2f796696c07f6685fa90da5b3459bb9cb4d285a0
Byeongdu Lee <35266961+byeongdu@users.noreply.github.com>
2025-08-21 15:39:30 -0500

    Update AThexapod.py

--- AThexapod.py
+++ AThexapod.py
@@ -74,13 +74,15 @@ def fly(axis="X", start=0, final=1, time=5):
     N_pulses = int(abs(final - start) / step_distance)  # Calculate the number of pulses to fire
     print(f" Done. Fly to {final} in {time} seconds. PSO generates pulses every {time/N_pulses} seconds and total {N_pulses} pulses.")
     epics.caput(command_PV, f'PsoDistanceEventsOn(ST1)') # Turn on PSO
+    print(epics.caget(f'{IOC_prefix}{motorpv}.DMOV'))
+    print("before start?")
     epics.caput(command_PV, f'MoveAbsolute({axis}, {final}, {abs(final-start)/time})') # Move ST1 to the specified position
     # Wait a moment to ensure the move has started
     while not epics.caget(f'{IOC_prefix}{motorpv}.DMOV'):
         print(epics.caget(f'{IOC_prefix}{motorpv}.DMOV'))
         sleep(0.02)
     print(epics.caget(f'{IOC_prefix}{motorpv}.DMOV'))
-    while epics.caget(f'{IOC_prefix}{motorpv}.DMOV'):
+    while epics.caget(f'{IOC_prefix}{motorpv}.DMOV')==1:
         print(epics.caget(f'{IOC_prefix}{motorpv}.DMOV'))
         sleep(0.02)
     epics.caput(command_PV, f'PsoOutputOff(ST1)') # Turn off PSO 
