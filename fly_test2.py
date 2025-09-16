4d866bd676e8bfe41f91f70ce32f888abf369c51
Byeongdulee <45212115+Byeongdulee@users.noreply.github.com>
2025-09-16 09:12:40 -0500

    Update fly_test2.py

--- fly_test2.py
+++ fly_test2.py
@@ -20,12 +20,13 @@ else:
 s12softglue.set_count_freq(10)
 s12softglue.ckTime_reset()
 s12softglue.memory_clear()
+det.fly_ready(0.001, 10, 10)
+#struck.mcs_init()
+#struck.mcs_ready(5000, 100)
+#struck.arm_mcs()
 
-struck.mcs_init()
-struck.mcs_ready(5000, 100)
-struck.arm_mcs()
-
-hp.set_traj(axis=['X', 'Z'], start=[0,0], final=[0.05, 0.01], Y_step = 0.002, time_per_line=1, pulse_step = 0.001, wait=True)
+#hp.set_traj(axis=['X', 'Z'], start=[0,0], final=[0.01, 0.01], Y_step = 0.000_5, time_per_line=1, pulse_step = 0.000_5, wait=True)
+hp.set_traj(axis=['X', 'Z'], start=[0,0], final=[1, 1], Y_step = 0.05, time_per_line=1, pulse_step = 0.05, wait=True)
 hp.run_traj()
 
 s12softglue.flush()
